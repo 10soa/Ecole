564bb01c4e1aaa9f0f4295c24adc8363d694a84a
Soa <soaornella5@gmail.com>
2022-03-09 20:28:16 +0300

    base

---
+++ base/data_ecole1.sql
@@ -0,0 +1,264 @@
+
+
+
+
+insert into "admin"
+values (nextval('adminseq'),md5('indro'),'admin1');
+
+insert into secretaire
+values (nextval('secre'),'Andry',md5('indro'),'secre1');
+
+insert into prof
+values (nextval('profseq'),'RAZAFINJOELINA Tahiana',md5('indro'),'rata');
+
+insert into prof
+values (nextval('profseq'),'NAINA Vahatra',md5('indro'),'naina');
+
+insert into prof
+values (nextval('profseq'),'RABENANAHARY Rojo',md5('indro'),'rojo');
+
+insert into prof
+values (nextval('profseq'),'RINDRA Tsiry',md5('indro'),'rojo');
+
+
+
+
+insert into promotion
+values (nextval('prom'),'p12',0,1);
+
+insert into promotion
+values (nextval('prom'),'p13',0,1);
+
+insert into promotion
+values (nextval('prom'),'p14',0,1);
+
+insert into promotion
+values (nextval('prom'),'p15',0,0);
+
+
+insert into niveau
+values (nextval('niv'),'S1',450000);
+
+insert into niveau
+values (nextval('niv'),'S3',400000);
+
+insert into niveau
+values (nextval('niv'),'S5',400000);
+
+insert into niveau
+values (nextval('niv'),'S7',500000);
+
+
+
+CREATE SEQUENCE nom;
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO','Modeste',1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'RAKOTO'||nextval('nom'),'Modeste'||nextval('nom'),1,4,'1998-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+
+insert into etudiant
+values (nextval('etu'),'ANDRIA'||nextval('nom'),'Paoly'||nextval('nom'),2,3,'2000-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+insert into etudiant
+values (nextval('etu'),'ANDRIA'||nextval('nom'),'Paoly'||nextval('nom'),2,3,'2000-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+insert into etudiant
+values (nextval('etu'),'ANDRIA'||nextval('nom'),'Paoly'||nextval('nom'),2,3,'2000-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+insert into etudiant
+values (nextval('etu'),'ANDRIA'||nextval('nom'),'Paoly'||nextval('nom'),2,3,'2000-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+insert into etudiant
+values (nextval('etu'),'ANDRIA'||nextval('nom'),'Paoly'||nextval('nom'),2,3,'2000-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+insert into etudiant
+values (nextval('etu'),'ANDRIA'||nextval('nom'),'Paoly'||nextval('nom'),2,3,'2000-12-08','etu00'||currval('etu'),'m','117 B Ville');
+
+
+
+insert into etudiant
+values (nextval('etu'),'MANJAKA'||nextval('nom'),'Feno'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'MANJAKA'||nextval('nom'),'Feno'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'MANJAKA'||nextval('nom'),'Feno'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'MANJAKA'||nextval('nom'),'Feno'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),3,2,'2002-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+
+
+insert into etudiant
+values (nextval('etu'),'TAFITA'||nextval('nom'),'Lanto'||nextval('nom'),4,1,'2002-12-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TAFITA'||nextval('nom'),'Lanto'||nextval('nom'),4,1,'2002-02-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TAFITA'||nextval('nom'),'Lanto'||nextval('nom'),4,1,'2002-10-15','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TAFITA'||nextval('nom'),'Lanto'||nextval('nom'),4,1,'2002-10-12','etu00'||currval('etu'),'f','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),4,1,'2003-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),4,1,'2004-01-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),4,1,'2003-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'TIMOTY'||nextval('nom'),'Valy'||nextval('nom'),4,1,'2003-02-15','etu00'||currval('etu'),'m','117 B Ville');
+
+insert into etudiant
+values (nextval('etu'),'MAOMY','Simone',4,1,'2000-05-15','etu00'||currval('etu'),'f','117 B Ville');
+
+
+
+insert into etudiantcorbeille
+values (nextval('drop'),35);
+
+insert into matiere
+values (nextval('mat'),'programmation',5,1,1,5);
+insert into matiere
+values (nextval('mat'),'programmation',7,2,1,15);
+insert into matiere
+values (nextval('mat'),'programmation',7,3,1,20);
+insert into matiere
+values (nextval('mat'),'programmation',10,4,1,30);
+
+insert into matiere
+values (nextval('mat'),'socket',7,2,2,1);
+insert into matiere
+values (nextval('mat'),'prog système',7,3,2,3);
+insert into matiere
+values (nextval('mat'),'système réseau',10,4,2,5);
+
+
+insert into matiere
+values (nextval('mat'),'Algèbre relationnelle',4,1,3,2);
+insert into matiere
+values (nextval('mat'),'WEB',6,2,3,2);
+insert into matiere
+values (nextval('mat'),'BDD',7,3,3,2);
+insert into matiere
+values (nextval('mat'),'WEB dynamique',8,4,3,2);
+
+
+
+insert into matiere
+values (nextval('mat'),'réseau informatique',3,1,4,1);
+insert into matiere
+values (nextval('mat'),'linux',5,2,4,2);
+insert into matiere
+values (nextval('mat'),'réseau informatique',5,3,4,2);
+insert into matiere
+values (nextval('mat'),'réseau avancée',7,4,4,2);
+
+
+
+        
+
+insert into note
+values (nextval('noteseq'),2,4,10,'2022-01-15');
+insert into note
+values (nextval('noteseq'),4,4,5,'2022-01-15');
+insert into note
+values (nextval('noteseq'),5,4,15,'2022-01-15');
+insert into note
+values (nextval('noteseq'),6,4,20,'2022-01-15');
+insert into note
+values (nextval('noteseq'),7,4,20,'2022-01-15');
+
+insert into note
+values (nextval('noteseq'),2,11,10,'2022-02-10');
+insert into note
+values (nextval('noteseq'),4,11,11.5,'2022-02-10');
+insert into note
+values (nextval('noteseq'),5,11,9,'2022-02-10');
+insert into note
+values (nextval('noteseq'),6,11,17,'2022-02-10');
+insert into note
+values (nextval('noteseq'),7,11,18,'2022-02-10');
+    
+
+
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),34,450000,2,2020,'2020-02-15',1);
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),33,300000,2,2020,'2020-02-10',1);
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),33,150000,2,2020,'2020-03-20',1);
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),34,200000,3,2020,'2020-03-10',1);
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),34,250000,3,2020,'2020-03-30',1);
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),30,450000,2,2020,'2020-02-10',1);
+insert into ecolageetudiant (id,idetudiant,montant,mois,annee,"date",idniveau)
+values (nextval('eco'),29,450000,2,2020,'2020-03-18',1);
+
+
+insert into notevalide values (nextval('notev'),1);
+insert into notevalide values (nextval('notev'),3);
+insert into notevalide values (nextval('notev'),4);
+insert into notevalide values (nextval('notev'),5);
+insert into notevalide values (nextval('notev'),6);
+insert into notevalide values (nextval('notev'),7);
+insert into notevalide values (nextval('notev'),9);
+insert into notevalide values (nextval('notev'),10);
